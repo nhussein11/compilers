e2fa06d35fa313c312850d330323bd707bf58b1a
nhussein11 <nicolashussein14@gmail.com>
2022-09-11 18:19:03 -0300

    add: separated tokens file

--- proyecto-final/tokens.py
+++ proyecto-final/tokens.py
@@ -1,32 +1,36 @@
-import ply.lex as lex
+reserved = {
+    "PINICIO": "INICIODEPROGRAMA",
+    "PEND": "FINPROGRAMA",
+    "extend": "LIBRERIA",
+    "VBLE": "VARIABLE",
+    "FUN": "FUNCION",
+    "PROC": "PROCEDIMIENTO",
+    "VPIN": "VPIN",
+    "BEGIN": "BEGIN",
+    "END": "END",
+    "IF": "IF",
+    "ELSE": "ELSE",
+    "WHILE": "WHILE",
+    "ADELANTE": "ADELANTE",
+    "ATRAS": "ATRAS",
+    "IZQUIERDA": "IZQUIERDA",
+    "DERECHA": "DERECHA",
+    "ESPERAR": "ESPERAR",
+    "FRENAR": "FRENAR",
+}
 
-tokens = ["INICIOPROGRAMA", "FINPROGRAMA", "PARENTESIS", "COMA", "EXTEND", "ID", "LIBRERIA", "VARIABLE", "VARTYPESEPARATOR", "TYPE", "NUMBER", "DECIMALNUMBER", "OPERADOR", "FUN", "PROC", "VPIN",
-          "COMMENT", "BEGIN", "END", "IF", "ELSE", "WHILE", "ADELANTE", "ATRAS", "IZQUIERDA", "DERECHA", "ESPERAR", "FRENAR"]
-
-
-def t_INICIOPROGRAMA(t):
-    r"PINICIO"
-    return t
-
-
-def t_FINPROGRAMA(t):
-    r"PEND"
-    return t
-
-
-def t_EXTEND(t):
-    r"EXTEND"
-    return t
-
-
-def t_VARIABLE(t):
-    r"VBLE"
-    return t
-
-
-def t_ID(t):
-    r'[a-zA-Z][a-zA-Z0-9]*'
-    return t
+tokens = [
+    "PARENTESIS",
+    "COMA",
+    "ID",
+    "LIBRERIA",
+    "VARTYPESEPARATOR",
+    "TYPE",
+    "NUMBER",
+    "DECIMALNUMBER",
+    "OPERADOR",
+    "COMMENT",
+] + list(reserved.values())
 
 
 def t_VARTYPESEPARATOR(t):
@@ -35,39 +39,30 @@ def t_VARTYPESEPARATOR(t):
 
 
 def t_TYPE(t):
-    r"int|float|String|bool|INPUT|OUTPUT"
+    r"entero|texto|decimal|logico|INPUT|OUTPUT"
+    return t
+
+
+def t_ID(t):
+    r"[a-zA-Z_][a-zA-Z0-9]*"
+    t.type = reserved.get(t.value, "ID")
     return t
 
 
 def t_NUMBER(t):
-    r'\d+'
+    r"\d+"
     t.value = int(t.value)
     return t
 
 
 def t_DECIMALNUMBER(t):
-    r'\d+\.\d+'
+    r"\d+\.\d+"
     t.value = float(t.value)
     return t
 
 
 def t_OPERADOR(t):
-    r'[+|-|*|>|<|>=|<=|==|!=|&&|.]'
-    return t
-
-
-def t_FUN(t):
-    r"FUN"
-    return t
-
-
-def t_PROC(t):
-    r"PROC"
-    return t
-
-
-def t_VPIN(t):
-    r"VPIN"
+    r"[+|-|*|>|<|>=|<=|==|!=|&&|.]"
     return t
 
 
@@ -77,92 +72,23 @@ def t_COMMENT(t):
     return t
 
 
-def t_BEGIN(t):
-    r"BEGIN"
-    return t
-
-
-def t_END(t):
-    r"END"
-    return t
-
-
-def t_IF(t):
-    r"IF"
-    return t
-
-
-def t_ELSE(t):
-    r"ELSE"
-    return t
-
-
-def t_WHILE(t):
-    r"WHILE"
-    return t
-
-
-def t_ADELANTE(t):
-    r"ADELANTE"
-    return t
-
-
-def t_ATRAS(t):
-    r"ATRAS"
-    return t
-
-
-def t_IZQUIERDA(t):
-    r"IZQUIERDA"
-    return t
-
-
-def t_DERECHA(t):
-    r"DERECHA"
-    return t
-
-
-def t_ESPERAR(t):
-    r"ESPERAR"
-    return t
-
-
-def t_FRENAR(t):
-    r"FRENAR"
-    return t
-
-
 def t_PARENTESIS(t):
-    r'[\(|\)]+'
+    r"[\(|\)]+"
     return t
 
 
 def t_COMA(t):
-    r','
+    r","
     return t
 
 
 def t_newline(t):
-    r';\n|\n|;'
+    r";\n|\n|;"
     t.lexer.lineno += 1
 
 
 def t_error(t):
-    print("Se encontró un error: %s" %
-          repr(t.value[0]) + " en la linea " + str(t.lineno))
+    print(
+        "Se encontró un error: %s" % repr(t.value[0]) + " en la linea " + str(t.lineno)
+    )
     t.lexer.skip(1)
-
-
-try:
-    with open('prog-ejemplo.arducompi', "r") as file:
-        data = file.read()
-    lexer = lex.lex()
-    lexer.input(data)
-    print("Token - Lexema")
-    while True:
-        tok = lexer.token()
-        if not tok:
-            break
-        print(f"( {tok.type}, '{tok.value}' )")
-except IndexError:
-    print('Error al leer el archivo')
